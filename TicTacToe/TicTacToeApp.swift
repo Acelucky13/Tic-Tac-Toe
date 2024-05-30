431fcbd22c26362190dd002e6a73adba4d46c6fc
Acelucky13 <Acelucky13@ymail.com>
2024-05-30 13:37:01 -0700

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Mike Gomez on 5/30/24.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
